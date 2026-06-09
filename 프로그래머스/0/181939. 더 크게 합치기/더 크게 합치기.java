eaefa3f6f8333830891f7993c83d2c95e071662a
JinsanPark <jmfa34@gmail.com>
2026-06-09 12:42:19 +0900

    [level 0] Title: 더 크게 합치기, Time: 6.57 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b) {
+    
+        int x = Integer.parseInt("" + a + b);
+        int y = Integer.parseInt("" + b + a);
+        int answer = 0;
+        
+        if (y > x) {
+            answer = y;
+        }  else {
+            answer = x;
+        }
+        
+        return answer;
+    }
+}
