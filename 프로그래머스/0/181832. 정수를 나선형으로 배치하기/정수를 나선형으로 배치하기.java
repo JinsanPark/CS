3f87fb2b85cc42ad3736fbd0eaf61e4b9cf5df47
JinsanPark <jmfa34@gmail.com>
2026-06-10 23:07:50 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.10 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -2,21 +2,24 @@ class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
         int[][] direction = {{0,1},{1,0},{0,-1},{-1,0}};
-        int row = 0;
-        int col = 0;
+        int curRow = 0;
+        int curCol = 0;
         int d = 0;
+        int nextRow = curRow;
+        int nextCol = curCol;
 
-        for(int i = 1; i <= n*n; i++) {
-
-            answer[row][col] = i;
-
-            if(0 <= row + direction[d][0] && row + direction[d][0] < n && 0 <= col + direction[d][1] && col + direction[d][1] < n && answer[row + direction[d][0]][col + direction[d][1]] == 0) {
-                row = row + direction[d][0];
-                col = col + direction[d][1];
+        for(int i = 1; i <= n*n;) {
+            if(0 <= nextRow && nextRow < n && 0 <= nextCol && nextCol < n && answer[nextRow][nextCol] == 0) {
+                answer[nextRow][nextCol] = i;
+                curRow = nextRow;
+                curCol = nextCol;
+                nextRow = curRow + direction[d][0];
+                nextCol = curCol + direction[d][1];
+                i++;
             } else {
                 d = (d + 1) % 4;
-                row = row + direction[d][0];
-                col = col + direction[d][1];
+                nextRow = curRow + direction[d][0];
+                nextCol = curCol + direction[d][1];
             }    
         }
 
