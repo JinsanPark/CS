56c20df3500773a6abb2960392a03b30860e651e
JinsanPark <jmfa34@gmail.com>
2026-07-03 01:10:36 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.18 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        char[] c = my_string.toCharArray();
+        
+        for(int i = 0; i < c.length; i++){
+                sb.append(("" + c[i]).repeat(n));
+        }
+        
+        return sb.toString();
+    }
+}
