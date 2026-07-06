058baa1a211d0f424c77dc4a10a07e69bc9c8b0a
JinsanPark <jmfa34@gmail.com>
2026-07-06 11:37:36 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int f = 1;
+        
+        for(int i = 1; i <= 11; i++){
+        
+            f *= i;
+            if(f > n){
+                answer = i - 1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
