028f0968d0e7ac4c3afacc7b3b3f9bae820c60d6
JinsanPark <jmfa34@gmail.com>
2026-07-10 02:03:01 +0900

    [level 0] Title: OX퀴즈, Time: 0.13 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        
+        for(int i = 0; i < quiz.length; i++){
+            String[] str = quiz[i].split(" ");
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[2]);
+            int c = Integer.parseInt(str[4]);
+            
+            if(str[1].equals("+")){
+                a += b;
+            } else {
+                a -= b;
+            }
+            
+            if(a == c){
+                answer[i] = "O";
+            } else {
+                answer[i] = "X";
+            }
+            
+        }
+        return answer;
+    }
+}
