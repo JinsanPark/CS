6c1903c805a2177021cfe53dda7aac7b1a9c452d
JinsanPark <jmfa34@gmail.com>
2026-07-09 20:34:29 +0900

    [level 0] Title: 약수 구하기, Time: 0.21 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int n) {
+        
+        int len = 0;
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                len++;
+            }
+        }
+        
+        int[] answer = new int[len];
+        int idx = 0;
+        
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                answer[idx++] = i;
+            }
+        }
+        
+        return answer;
+    }
+}
