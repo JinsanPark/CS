ac7bc7d326a3299600ff2340c1290cf1cc5503ad
JinsanPark <jmfa34@gmail.com>
2026-07-23 21:04:43 +0900

    [level 0] Title: 이진수 더하기, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(String bin1, String bin2) {
         StringBuilder sb = new StringBuilder();
-        int len = bin1.length() >= bin2.length() ? bin1.length() : bin2.length();
         int round = 0;
         int idx1 = bin1.length() - 1;
         int idx2 = bin2.length() - 1;
-        int i = 0;
 
         while(idx1 >= 0 || idx2 >= 0 || round != 0){
             
@@ -17,13 +15,10 @@ class Solution {
             if(idx2 >= 0){
                 count += bin2.charAt(idx2--) - '0';
             } 
-            if(round == 1){
-                count++;
-            }
+        
+            count += round;
             round = count / 2;
             sb.append(count % 2);
-            i++;
-    
         }
         return sb.reverse().toString();
     }
