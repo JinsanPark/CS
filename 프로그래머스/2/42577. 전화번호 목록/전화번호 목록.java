487e1ef9884197199f21a71cb2a30719d4af6a79
JinsanPark <jmfa34@gmail.com>
2026-08-03 04:18:05 +0900

    [level 2] Title: 전화번호 목록, Time: 183.69 ms, Memory: 229 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Set<String> set = new HashSet<>();
+        
+        for(int i = 0; i < phone_book.length; i++){
+            set.add(phone_book[i]);
+        }
+        
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 1; j < phone_book[i].length(); j++){
+                String str = phone_book[i].substring(0, j);
+                
+                if(set.contains(str)){
+                    return false;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
